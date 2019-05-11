0f8b38e047d023090859bee79e7b6cbf3444029d
jimsshom <jimsshom@gmail.com>
2019-05-11 21:10:22 +0800

    前后端基本调通，还差样式

---
+++ mac_client/background.js
@@ -0,0 +1,16 @@
+document.write('test')
+var ipcRenderer = require('electron').ipcRenderer
+var request = require('request')
+//ipcRenderer.send('getdata', [Math.round((new Date()).getTime() / 1000), 'test'])
+
+var timerID = setInterval(function() {
+    request('http://10.0.0.7:8080/get_plantower_data', function (error, response, body) {
+        console.log(error)
+        console.log(response)
+        console.log(body)
+        ipcRenderer.send('getdata', [Math.round((new Date()).getTime() / 1000), body])
+    })
+
+    // testdata = '51|58|60|47|65|55|12915|3576|533|46|8|2|0|235|467|0'
+    // ipcRenderer.send('getdata', [Math.round((new Date()).getTime() / 1000), testdata])
+}, 5 * 1000); 
--- mac_client/main.js
+++ mac_client/main.js
@@ -1,19 +1,74 @@
+const electron = require('electron')
 const {app, BrowserWindow, Tray, Menu, nativeImage, ipcMain} = require('electron')
 
-let mainWindow
 let appIcon
 let backWindow
+let dispWindow
+let currentData
 const lowimage = nativeImage.createFromPath('/Users/jimsshom/Works/GitRepo/PlantowerMonitor/mac_client/low.png')
 const middleimage = nativeImage.createFromPath('/Users/jimsshom/Works/GitRepo/PlantowerMonitor/mac_client/middle.png')
 const highimage = nativeImage.createFromPath('/Users/jimsshom/Works/GitRepo/PlantowerMonitor/mac_client/high.png')
 
-function createBackWindow() {
-    backWindow = new BrowserWindow({
+function createDisplayWindow(x, y) {
+    dispWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        x: x,
+        y: y,
         show: false,
+        frame: false,
         webPreferences: {
-            nodeIntegrationInWorker: true
+            nodeIntegration: true
         }
     })
+    //dispWindow.webContents.openDevTools()
+    dispWindow.loadFile('display.html')
+    dispWindow.once('ready-to-show', () => {
+        dispWindow.webContents.send('showdata', currentData)
+        dispWindow.show()
+    })
+    dispWindow.on('close', () => {
+        dispWindow = null
+    })
+}
+
+function closeDisplayWindow() {
+    if (dispWindow != null) {
+        dispWindow.close()
+    }
+}
+
+function triggerDisplayWindow(x, y) {
+    if (dispWindow == null) {
+        createDisplayWindow(x, y)
+    } else {
+        closeDisplayWindow()
+    }
+}
+
+function createBackWindow() {
+    let displays = electron.screen.getAllDisplays()
+    let externalDisplay = displays.find((display) => {
+        return display.bounds.x !== 0 || display.bounds.y !== 0
+    })
+    if (externalDisplay) {
+        backWindow = new BrowserWindow({
+            x: externalDisplay.bounds.x + 800,
+            y: externalDisplay.bounds.y + 400,
+            show: true,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        })
+    } else {
+        backWindow = new BrowserWindow({
+            show: true,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        })
+    }
+    backWindow.webContents.openDevTools()
     backWindow.loadFile('background.html')
 }
 
@@ -26,17 +81,48 @@ function createTray() {
     ])
     appIcon.setTitle('waiting')
     appIcon.setToolTip('just for test')
-    appIcon.setContextMenu(contextMenu)
+    //appIcon.setContextMenu(contextMenu)
+    appIcon.on('click', (event, bounds, position) => {
+        //新建窗口并且实时查询
+        //createDisplayWindow()
+        console.log(bounds)
+        console.log(position)
+        let x = bounds.x
+        let y = bounds.height
+        triggerDisplayWindow(x, y)
+    })
+    appIcon.on('mouse-leave', (event, position) => {
+        //关闭窗口
+        //closeDisplayWindow()
+    })
 }
 
 app.on('ready', () => {
-    ipcMain.on('getdata', (event, arg) => {
-      appIcon.setTitle(arg[0].toString(10))
-      appIcon.setImage(highimage)
-      mainWindow.webContents.send('showdata', arg)
-    })
-  
     createTray()
+    ipcMain.on('getdata', (event, arg) => {
+        //CF=1(ug/m3) PM1.0/PM2.5/PM10
+        //STD(ug/m3)  PM1.0/PM2.5/PM10
+        //>(/0.1L)    0.3um/0.5um/1.0um/2.5um/5.0um/10um
+        //HCHO  Temperature   Humidity
+        ts = arg[0]
+        data = arg[1]
+        var [cf1_10,cf1_25,cf1_100,std_10,std_25,std_100,um_3,um_5,um_10,um_25,um_50,um_100,hcho,temp,hum,check] = data.split('|')
+        var aqi = Math.max(cf1_10, cf1_25, cf1_100, std_10, std_25, std_100)
+        if (aqi > 120) {
+            appIcon.setImage(highimage)
+        } else if (aqi > 70) {
+            appIcon.setImage(middleimage)
+        } else {
+            appIcon.setImage(lowimage)
+        }
+        appIcon.setTitle(aqi.toString(10))
+        //mainWindow.webContents.send('showdata', arg)
+        currentData = arg
+        if (dispWindow != null) {
+            dispWindow.webContents.send('showdata', currentData)
+        }
+    })
+    createBackWindow()
 })
 
 app.on('window-all-closed', function () {
